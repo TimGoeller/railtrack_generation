c185819cd8bdda268077c7dbfbd85371cbd8dff0
Tim Göller <tim.goeller@posteo.de>
2020-01-03 16:23:38 +0100

    Add RailTrackEditor editor ui displayment

--- Assets/Scripts/Editor/RailTrackEditor.cs
+++ Assets/Scripts/Editor/RailTrackEditor.cs
@@ -14,4 +14,20 @@ public class RailTrackEditor : Editor
     {
         railTrack = target as RailTrack;
     }
+
+    public override void OnInspectorGUI()
+    {
+        EditorGUI.BeginChangeCheck();
+
+        railTrack.trackWidth = EditorGUILayout.FloatField("Track Width", railTrack.trackWidth);
+        railTrack.segmentCount = EditorGUILayout.IntField("Segment Count", railTrack.segmentCount);
+        railTrack.spaceBetweenSegments = EditorGUILayout.FloatField("Space Between Segements", railTrack.spaceBetweenSegments);
+        railTrack.trackOffsetPercentage = EditorGUILayout.Slider("Track Offset", railTrack.trackOffsetPercentage, 0.05f, 0.25f);
+
+        if(EditorGUI.EndChangeCheck())
+        {
+            //TODO
+        }
+
+    }
 }
--- Assets/Scripts/RailTrack.cs
+++ Assets/Scripts/RailTrack.cs
@@ -7,8 +7,8 @@ public class RailTrack : MonoBehaviour
 {
     #region Attributes
     public float trackWidth = 2;
-    public float trackOffsetPercentage = 0.15;
-    public float segmentCount = 10;
-    public float spaceBetweenSegments = 0.5;
+    public int segmentCount = 10;
+    public float spaceBetweenSegments = 0.5f;
+    public float trackOffsetPercentage = 0.15f;
     #endregion
 }
