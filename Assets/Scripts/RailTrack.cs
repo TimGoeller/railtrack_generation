39a02640754863e42f4e83a8744088d93e2cd53d
Tim Göller <tim.goeller@posteo.de>
2020-01-03 15:38:27 +0100

    Add RailTrack with initial parameters

---
+++ Assets/Scripts/RailTrack.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RailTrack : MonoBehaviour
+{
+    #region Attributes
+    public float trackWidth = 2;
+    public float trackOffsetPercentage = 0.15;
+    public float segmentCount = 10;
+    public float spaceBetweenSegments = 0.5;
+    #endregion
+}
