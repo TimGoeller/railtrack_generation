b33b8a331f685549f00dae4857e7899113480476
Tim Göller <tim.goeller@posteo.de>
2020-01-03 15:38:52 +0100

    Add RailTrackEditor with target assignment

---
+++ Assets/Scripts/Editor/RailTrackEditor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(RailTrack))]
+public class RailTrackEditor : Editor
+{
+    #region Attributes
+    RailTrack railTrack;
+    #endregion
+
+    private void Awake()
+    {
+        railTrack = target as RailTrack;
+    }
+}
